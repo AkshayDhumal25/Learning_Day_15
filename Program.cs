784371cd865beb72055de4357e2167b48b085aec
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-04-03 09:47:04 +0530

    // 3. Logic for anagram program with its time complexity. (for large strings).

--- Program.cs
+++ Program.cs
@@ -32,6 +32,47 @@
 
 
 // 2. WAP to reverse an integer without converting it to a string, without using any built-in methods.
+//using System;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            int n = 123;
+//            int reversed = 0;
+//            while (n > 0)
+//            {
+//                int reminder = n % 10;
+//                reversed = reminder + reversed * 10;
+//                n /= 10;
+//            }
+//            Console.WriteLine(reversed);
+//        }
+//    }
+//}
+
+// 2. Swap Values without using any variable a=10, b=12.
+//using System;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            int a = 10;
+//            int b = 12;
+
+//            a = a + b;
+//            b = a - b;
+//            a = a - b;
+
+//            Console.WriteLine($"A: {a} and B: {b}");
+//        }
+//    }
+//}
+
+// 3. Logic for anagram program with its time complexity. (for large strings).
 using System;
 namespace logicals
 {
@@ -39,15 +80,36 @@ namespace logicals
     {
         public static void Main(string[] args)
         {
-            int n = 123;
-            int reversed = 0;
-            while (n > 0)
+            string str1 = "silemt";
+            string str2 = "listen";
+
+            if (isAnagram(str1, str2))
             {
-                int reminder = n % 10;
-                reversed = reminder + reversed * 10;
-                n /= 10;
+                Console.WriteLine("Anagram");
             }
-            Console.WriteLine(reversed);
+            else
+            {
+                Console.WriteLine("Not Anagram");
+            }
+        }
+
+        public static bool isAnagram(string str1, string str2)
+        {
+            int[] array = new int[256];
+            for (int i = 0; i < str1.Length; i++)
+            {
+                array[str1[i]]++;
+                array[str2[i]]--;
+            }
+
+            foreach (var i in array)
+            {
+                if (i != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
